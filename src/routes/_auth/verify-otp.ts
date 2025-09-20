2dc947d65e873ed527c1949231db878bfc3f1694
Maina <mainaedwin913@gmail.com>
2025-09-20 11:41:37 +0300

    updates: before load on veiry otp

--- src/routes/_auth/verify-otp.ts
+++ src/routes/_auth/verify-otp.ts
@@ -1,7 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { OtpPage } from '@/core/features/pages/auth/VerifyOtpPage'
 
 export const Route = createFileRoute('/_auth/verify-otp')({
+  beforeLoad: ({ location }) => {
+    const token =
+      typeof window !== 'undefined' ? sessionStorage.getItem('OtpToken') : null
+
+    if (!token) {
+      throw redirect({
+        to: '/login',
+        search: {
+          redirect: location.pathname,
+        },
+      })
+    }
+  },
   component: OtpPage,
 })
-
