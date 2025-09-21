fd6a9f49bc25448602f681a3c9f59df4554636ef
Maina <mainaedwin913@gmail.com>
2025-09-22 01:10:27 +0300

    updates: session on login

--- src/routes/_auth/login.ts
+++ src/routes/_auth/login.ts
@@ -1,8 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { LoginPage } from '@/core/features/pages/auth/LoginPage'
-
+import { SessionClient } from '@/core/lib/session.client'
 
 export const Route = createFileRoute('/_auth/login')({
+   beforeLoad: ({ location }) => {
+      const token = SessionClient.getToken()
+      const expired = SessionClient.isTokenExpired()  
+  
+      if (token && !expired) {
+        throw redirect({
+          to: '/dashboard',
+          search: { redirect: location.pathname },
+        })
+      }
+    },
   component: LoginPage,
 })
 
